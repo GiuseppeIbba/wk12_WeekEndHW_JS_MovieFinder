479d741cf5f93ea892396298bd40fd42d0e75f84
Giuseppe <giuseppe.ibba@hotmail.com>
2018-02-24 15:38:47 +0000

    created a button and an input field

---
+++ public/app.js
@@ -0,0 +1,113 @@
+  const app = function(){
+
+
+  const mainDiv = document.createElement('div');
+  document.body.appendChild(mainDiv);
+
+  const topDiv = document.createElement('div');
+  const searchBar = document.createElement('input')
+  searchBar.placeholder = "Type a movie title";
+
+  const searchButton = document.createElement('button');
+  searchButton.innerHTML = "Search";
+  searchButton.addEventListener('click', function(){
+  const search = searchBar.value;
+  
+  });
+
+  mainDiv.appendChild(topDiv);
+  mainDiv.appendChild(searchBar);
+  mainDiv.appendChild(searchButton);
+
+  // const searchTextAndLogoDiv = document.createElement('div');
+  // const searchTextDiv = document.createElement('div');
+  // const companyLogoDiv = document.createElement('div');
+  // mainDiv.appendChild(searchTextAndLogoDiv);
+  // searchTextAndLogoDiv.appendChild(searchTextDiv);
+  // searchTextAndLogoDiv.appendChild(companyLogoDiv);
+
+
+
+
+
+
+
+
+
+
+
+  // const topContentImg= document.createElement('img');
+  // topContentImg.src = "http://cdn.droidviews.com/wp-content/uploads/2016/10/4-Retro-Games-Android.jpg"
+  // stopContentImg.alt = "TOP IMAGE CONTENT IS NOT LINKED"
+
+  const topContentParagraph = document.createElement('p');
+  topContentParagraph.innerText = "FILM SEARCH WEEKEND PROJECT"
+  // topContentParagraph.appendChild(topContentImg);
+  topDiv.appendChild(topContentParagraph);
+
+
+  // const searchTextAndLogoDiv = document.createElement('div');
+  //
+  // const searchTextDiv = document.createElement('div');
+  // const companyLogoDiv = document.createElement('div');
+  //
+  //
+  // mainDiv.appendChild(searchTextAndLogoDiv);
+  // searchTextAndLogoDiv.appendChild(searchTextDiv);
+  // searchTextAndLogoDiv.appendChild(companyLogoDiv);
+
+
+
+
+
+
+  // console.log('funziona')
+}
+
+  /////////////////////////////////////////////////
+  ///USER INPUT
+  //////////////////////////////
+
+  // searchBar.innerText = ;
+
+
+  // searchButton.addEventListener('click', handleButtonClick);
+
+
+
+  const handleButtonClick = function(){
+    const search = searchBar.innerText;
+    document.querySelector('input').innerHTML = search;
+    const url = 'http://www.omdbapi.com/?t=' + title + '&apikey=24cd73c2';
+    makeRequest(url, requestComplete);
+  }
+
+const makeRequest = function(url, callback){
+  const request = new XMLHttpRequest();
+  request.open("Get",url);
+  // request.setRequestHeader("user-key", API_KEY);
+  // request.setRequestHeader("Accept", 'application/json');
+  request.addEventListener('load', callback);
+  request.send();
+}
+
+// const requestComplete = function(){
+//   if(this.status !== 200) return;
+//   const jsonString = this.responseText;
+//   const movies = JSON.parse(jsonString);
+//   // console.log(moviesMatchingSearch);
+//   filmsData(movies);
+// }
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", app);
+
+
+
+ // -----------------------
+
+
+  // finished html and start with js
