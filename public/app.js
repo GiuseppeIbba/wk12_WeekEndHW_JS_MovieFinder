30f4d35fa42a04721878969e2a43e9bfee819c07
Giuseppe <giuseppe.ibba@hotmail.com>
2018-02-25 12:27:22 +0000

    added functions to rietrieve and display details and img. Refactored some codelines

--- public/app.js
+++ public/app.js
@@ -1,73 +1,157 @@
-  const app = function(){
+const app = function(){
 
 
   const mainDiv = document.createElement('div');
   document.body.appendChild(mainDiv);
 
-  const topDiv = document.createElement('div');
-  const searchBar = document.createElement('input')
-  searchBar.placeholder = "Type a movie title";
-
-  const searchButton = document.createElement('button');
+  const topDiv = document.createElement('header');
   mainDiv.appendChild(topDiv);
-  mainDiv.appendChild(searchBar);
-  mainDiv.appendChild(searchButton);
 
-  searchButton.innerHTML = "Search";
-  searchButton.addEventListener('click', function(){
-  const movieTitle = searchBar.value
-  const space = / /gi;
-  const movieTitleModifier = movieTitle.replace(space, "+");
-  // movieTitleModifier = movieTitle.split(" ");
-  // console.log(movieTitleModifier);
-  const url = 'http://www.omdbapi.com/?s=' + movieTitleModifier + '&apikey=24cd73c2';
-  // const url2 = 'http://www.omdbapi.com/?type=' + movieTitleModifier + '&apikey=24cd73c2';
-  console.log(url);
-  // console.log(url2);
-  const request = new XMLHttpRequest();
-  const request2 = new XMLHttpRequest();
-  request.open("Get", url);
-  // request.open("Get", url);
-  request.addEventListener('load', function () {
-  const moviesRetrieved = JSON.parse(request.responseText);
-  const movies = moviesRetrieved.Search;
-  // console.log(movies);
-  extractMovies(movies);
+  const displayDetailsAndImgDiv = document.createElement('div');
+  mainDiv.appendChild(displayDetailsAndImgDiv);
 
-  // request.send();
-  // console.log(movies);
-  })
-  request.send();
-})
+  const searchBarAndButtonTitleDiv = document.createElement('div');
+  topDiv.appendChild(searchBarAndButtonTitleDiv);
 
-const extractMovies = function(movies){
-  if (movies !== undefined) {
-  movies.forEach(function(movie){
-    movie
-      console.log(movie);
-});
+  const searchBarTitleDiv = document.createElement('input')
+  searchBarTitleDiv.placeholder = "Type a movie title";
+  const searchButtonTitleDiv = document.createElement('button');
+  searchBarAndButtonTitleDiv.appendChild(searchBarTitleDiv);
+  searchBarAndButtonTitleDiv.appendChild(searchButtonTitleDiv);
+
+  const displayDetailsDiv = document.createElement('div');
+  const displayImageDiv = document.createElement('div');
+  displayDetailsAndImgDiv.appendChild(displayDetailsDiv);
+  displayDetailsAndImgDiv.appendChild(displayImageDiv);
+
+  const detailsUl = document.createElement('ul');
+  displayDetailsDiv.appendChild(detailsUl);
+
+  mainDiv.id = "mainDiv";
+  topDiv.id = "topDiv";
+  displayDetailsAndImgDiv.id = "displayDetailsAndImgDiv";
+  displayDetailsDiv.id = "displayDetailsDiv";
+  displayImageDiv.id = "displayImageDiv"
+  searchBarAndButtonTitleDiv.id = "searchBarAndButtonTitleDiv";
+  searchBarTitleDiv.id = "searchBarTitleDiv";
+  searchButtonTitleDiv.id = "searchButtonTitleDiv";
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  searchButtonTitleDiv.innerHTML = "Search";
+  searchButtonTitleDiv.addEventListener('click', function(){
+    const moviesTitle = searchBarTitleDiv.value
+    const space = / /gi;
+    const moviesTitleModifier = moviesTitle.replace(space, "+");
+    const url = 'http://www.omdbapi.com/?s=' + moviesTitleModifier + '&apikey=24cd73c2';
+    console.log(url);
+    const requestDetails = new XMLHttpRequest();
+    requestDetails.open("Get", url);
+    requestDetails.addEventListener('load', function () {
+      const moviesDetailsRetrieved = JSON.parse(requestDetails.responseText);
+      const moviesDetails = moviesDetailsRetrieved.Search;
+      extractMoviesDetails(moviesDetails);
+    });
+    requestDetails.send();
+  });
+  //
+  // searchButtonType.innerHTML = "Search";
+  // searchButtonType.addEventListener('click', function(){
+  //   const movieType = searchBarType.value
+  //   const space = / /gi;
+  //   const movieTypeModifier = movieType.replace(space, "+");
+  //   const url2 = 'http://www.omdbapi.com/?type=' + movieTypeModifier + '&apikey=24cd73c2';
+  //   console.log(url2);
+  //   const requestType = new XMLHttpRequest();
+  //   requestType.open("Get", url2);
+  //   requestType.addEventListener('load', function () {
+  //     const moviesTypeRetrieved = JSON.parse(requestType.responseText);
+  //     const moviesTypes = moviesTypeRetrieved.Search;
+  //     extractMoviesByType(moviesTypes);
+  //   });
+  //   requestType.send();
+  // });
+
+  const extractMoviesDetails = function(moviesDetails){
+    if (moviesDetails !== undefined) {
+      moviesDetails.forEach(function(movie){
+        displayTitle(movie.Title);
+        displayYear(movie.Year);
+        displayType(movie.Type);
+        displayPoster(movie.Poster);
+
+        console.log(movie);
+      });
+    };
+  };
+
+  const displayTitle = function(title){
+    const titleLi = document.createElement('Li')
+    titleLi.innerText = title;
+    detailsUl.appendChild(titleLi);
+  }
+
+  const displayYear = function(year){
+    const yearLi = document.createElement('Li')
+    yearLi.innerText = year;
+    detailsUl.appendChild(yearLi);
+  }
+
+  const displayType = function(type){
+    const typeLi = document.createElement('Li')
+    typeLi.innerText = type;
+    detailsUl.appendChild(typeLi);
+  }
+
+  const displayPoster = function(poster){
+    const image = document.createElement('img')
+    image.src = poster;
+    displayImageDiv.appendChild(image);
+
+
+  }
+  // const extractMoviesByType = function(moviesTypes){
+  //   if (moviesTypes !== undefined) {
+  //     moviesTypes.forEach(function(movie){
+  //       movie
+  //       console.log(movie);
+  //     });
+  //   };
+  // };
+
 }
-}
-}
-
-
-
-
-    // titleResults.forEach(function(title){}
 
 
 
 
 
 
-
-
-  // const searchTextAndLogoDiv = document.createElement('div');
-  // const searchTextDiv = document.createElement('div');
-  // const companyLogoDiv = document.createElement('div');
-  // mainDiv.appendChild(searchTextAndLogoDiv);
-  // searchTextAndLogoDiv.appendChild(searchTextDiv);
-  // searchTextAndLogoDiv.appendChild(companyLogoDiv);
+// titleResults.forEach(function(title){}
 
 
 
@@ -76,54 +160,68 @@ const extractMovies = function(movies){
 
 
 
-
-
-
-  // const topContentImg= document.createElement('img');
-  // topContentImg.src = "http://cdn.droidviews.com/wp-content/uploads/2016/10/4-Retro-Games-Android.jpg"
-  // stopContentImg.alt = "TOP IMAGE CONTENT IS NOT LINKED"
-
-  // const topContentParagraph = document.createElement('p');
-  // topContentParagraph.innerText = "FILM SEARCH WEEKEND PROJECT"
-  // // topContentParagraph.appendChild(topContentImg);
-  // topDiv.appendChild(topContentParagraph);
-
-
-  // const searchTextAndLogoDiv = document.createElement('div');
-  //
-  // const searchTextDiv = document.createElement('div');
-  // const companyLogoDiv = document.createElement('div');
-  //
-  //
-  // mainDiv.appendChild(searchTextAndLogoDiv);
-  // searchTextAndLogoDiv.appendChild(searchTextDiv);
-  // searchTextAndLogoDiv.appendChild(companyLogoDiv);
+// const displayDetailsAndImgDiv = document.createElement('div');
+// const displayDetails = document.createElement('div');
+// const companyLogoDiv = document.createElement('div');
+// mainDiv.appendChild(displayDetailsAndImgDiv);
+// displayDetailsAndImgDiv.appendChild(displayDetails);
+// displayDetailsAndImgDiv.appendChild(companyLogoDiv);
 
 
 
 
 
 
-  // console.log('funziona')
 
 
-  /////////////////////////////////////////////////
-  ///USER INPUT
-  //////////////////////////////
-
-  // searchBar.innerText = ;
 
 
-  // searchButton.addEventListener('click', handleButtonClick);
+
+// const topContentImg= document.createElement('img');
+// topContentImg.src = "http://cdn.droidviews.com/wp-content/uploads/2016/10/4-Retro-Games-Android.jpg"
+// stopContentImg.alt = "TOP IMAGE CONTENT IS NOT LINKED"
+
+// const topContentParagraph = document.createElement('p');
+// topContentParagraph.innerText = "FILM SEARCH WEEKEND PROJECT"
+// // topContentParagraph.appendChild(topContentImg);
+// topDiv.appendChild(topContentParagraph);
 
 
-  //
-  // const handleButtonClick = function(){
-  //   const search = searchBar.innerText;
-  //   document.querySelector('input').innerHTML = search;
-  //   const url = 'http://www.omdbapi.com/?t=' + title + '&apikey=24cd73c2';
-  //   makeRequest(url, requestComplete);
-  // }
+// const displayDetailsAndImgDiv = document.createElement('div');
+//
+// const displayDetails = document.createElement('div');
+// const companyLogoDiv = document.createElement('div');
+//
+//
+// mainDiv.appendChild(displayDetailsAndImgDiv);
+// displayDetailsAndImgDiv.appendChild(displayDetails);
+// displayDetailsAndImgDiv.appendChild(companyLogoDiv);
+
+
+
+
+
+
+// console.log('funziona')
+
+
+/////////////////////////////////////////////////
+///USER INPUT
+//////////////////////////////
+
+// searchBar.innerText = ;
+
+
+// searchButton.addEventListener('click', handleButtonClick);
+
+
+//
+// const handleButtonClick = function(){
+//   const search = searchBar.innerText;
+//   document.querySelector('input').innerHTML = search;
+//   const url = 'http://www.omdbapi.com/?t=' + title + '&apikey=24cd73c2';
+//   makeRequest(url, requestComplete);
+// }
 
 // const makeRequest = function(url, callback){
 //   const request = new XMLHttpRequest();
@@ -150,7 +248,7 @@ document.addEventListener("DOMContentLoaded", app);
 
 
 
- // -----------------------
+// -----------------------
 
 
-  // finished html and start with js
+// finished html and start with js
